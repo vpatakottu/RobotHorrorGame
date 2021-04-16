4195a2926bb4ade0f1539fa0ba3229f5de044e1e
Varshita Patakottu <varshita13@gmail.com>
2021-04-16 01:14:42 -0400

    modified loader

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e751742f2d9e916fa2d887347599577ce2be194b259a3dd96cd0abb894a7254
-size 16096
+oid sha256:7e0b8a026a62851aed4f863e02aee534e27bf376100df65886f0c037b40e9aaf
+size 16290
